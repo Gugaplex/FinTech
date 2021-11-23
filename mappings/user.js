027ff2d3082436c75aa803644d575267463ae09b
Gugaplex <tayshaoqi@u.nus.edu>
2021-11-23 18:14:31 +0800

    POST API New Account

--- mappings/user.js
+++ mappings/user.js
@@ -34,11 +34,27 @@ router.get("/user/by-id", (request, response) => {
   }
 });
 
-// // To add a new user to our database
-// router.post("/user/add", (request, response) => {
-//   let user = request.body;
-//   database.add_user(user);
-//   response.status(200).send("User added to the database!");
-// });
+// To add a new user to our database
+router.post("/user/add", (request, response) => {
+  connection.query(
+    `insert into user (first_name, last_name, email, phone, plan_id, signup_date)
+    values (
+      '${request.body.first_name}', 
+      '${request.body.last_name}',
+      '${request.body.email}', 
+      '${request.body.phone}',
+      '${request.body.plan_id}',
+      now())`,
+
+    (error, records) => {
+      if (error) {
+        console.log(error);
+        response.status(500).send("Some error ocurred while executing query");
+      } else {
+        response.status(200).send("User added to the database!");
+      }
+    }
+  );
+});
 
 module.exports = { router };
